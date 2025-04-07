be3f0f21cc64a77f72f7f4b095aea9d5081a9d83
Fabep <fabian-bechpersson@hotmail.com>
2025-04-07 10:39:05 +0200

    Added check for spaces in thirdword.

--- FlowControl/ThirdWord.cs
+++ FlowControl/ThirdWord.cs
@@ -9,10 +9,35 @@ internal class ThirdWord
     {
         Console.Clear();
         Console.WriteLine("Enter a sentence with at least 3 words");
+        bool finished = false;
+        string? thirdWord = null;
+        do
+        {
+            string sentence = Utils.PromptForString("Sentence", (x) => x.Split(' ').Length >= 3, "Input sentence is too short");
 
-        string sentence = Utils.PromptForString("Sentence", (x) => x.Split(' ').Length >= 3, "Input sentence is too short");
-        
-        string thirdWord = sentence.Split(' ')[2];
+            string[] arr = sentence.Split(' ');
+
+            int index = 2;
+            while (thirdWord is null && index < arr.Length)
+            {
+                string temp = arr[index];
+
+                if (string.IsNullOrWhiteSpace(temp))
+                {
+                    index++;
+                    continue;
+                }
+
+                thirdWord = arr[index];
+                finished = true;
+                break;
+            }
+
+            if (!finished)
+                Console.WriteLine("Invalid input. Input sentence is too short.");
+
+        }
+        while (!finished);
 
         Console.WriteLine($"The third word in your sentence is {thirdWord}!{Environment.NewLine}");
 
